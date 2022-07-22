d292287062325870bbd4b1705a83ef8704894f48
Piotr Zdebski <piotrek.zdebski@gmail.com>
2022-07-22 18:53:20 +0200

    update - task01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
+const galleryImages = galleryItems
+    .map((image) => {
+        const item = `<div class="gallery_item">
+        <a class="gallery_link" href="${image.original}">
+        <img
+        class="gallery_image"
+        src="${image.preview}"
+        data-src=${image.original}"
+        alt="${image.description}"
+        />
+        </a>
+        </div>`
+        return item;
+    })
+    .join(" ");
+
+gallery.insertAdjacentHTML('afterbegin', galleryImages);
+
 
 console.log(galleryItems);
