96c017b9dc5bf345e9b414c42c3dd3eecb0b5ef2
Piotr Zdebski <piotrek.zdebski@gmail.com>
2022-07-23 14:15:19 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
+const galleryImages = galleryItems
+    .map((image) => {
+        const item = `<div class="gallery__item">
+        <a class="gallery__link" href="${image.original}">
+        <img
+        class="gallery__image"
+        src="${image.preview}"
+        alt=${image.description}"
+        title="${image.description}"
+        />
+        </a>
+        </div>`
+        return item;
+    })
+    .join(" ");
+
+gallery.insertAdjacentHTML('afterbegin', galleryImages);
+
+new SimpleLightbox('.gallery a', {captionDelay: 250});
 
 console.log(galleryItems);
